6f00af0641104c346b72a296a797bb09b03bf02f
deniskorbakov <korbakovd@gmail.com>
2024-10-29 23:28:22 +0300

    dev: init project

---
+++ src/components/Layouts/AppLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Header from "../Commons/Header.vue";
+import SideBar from "../Commons/SideBar.vue";
+</script>
+<template>
+  <Header/>
+  <SideBar/>
+  <main>
+    <slot/>
+  </main>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import AppLayout from "../components/Layouts/AppLayout.vue";
+</script>
+
+<template>
+  <AppLayout>
+    <section>
+      this is home page
+    </section>
+  </AppLayout>
+</template>
+
+<style scoped>
+
+</style>
