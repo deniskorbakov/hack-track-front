d379c4e6868337467cd0fe14d00da72f576e21f2
deniskorbakov <korbakovd@gmail.com>
2024-10-30 23:20:49 +0300

    feat: create side bar

--- src/components/Commons/SideBar.vue
+++ src/components/Commons/SideBar.vue
@@ -1,13 +1,51 @@
 <script setup lang="ts">
-
+import {CubeIcon, PresentationChartLineIcon, CalendarDaysIcon, RectangleStackIcon, UserCircleIcon} from "@heroicons/vue/24/solid";
 </script>
 
 <template>
-  <nav>
-    navbar
-  </nav>
+  <aside>
+    <nav>
+      <ul class="links">
+        <li class="links-item links-item-active">
+          <router-link to="/">
+            <UserCircleIcon />
+          </router-link>
+        </li>
+        <li class="links-item">
+          <router-link to="/">
+            <CubeIcon />
+          </router-link>
+        </li>
+        <li class="links-item">
+          <router-link to="/">
+            <PresentationChartLineIcon />
+          </router-link>
+        </li>
+        <li class="links-item">
+          <router-link to="/">
+            <CalendarDaysIcon />
+          </router-link>
+        </li>
+        <li class="links-item">
+          <router-link to="/">
+            <RectangleStackIcon />
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </aside>
 </template>
 
-<style scoped>
+<style scoped lang="postcss">
+.links {
+  @apply mt-10 pl-1
+}
 
+.links-item {
+  @apply size-8 mb-8
+}
+
+.links-item-active {
+  @apply text-violet-600
+}
 </style>
