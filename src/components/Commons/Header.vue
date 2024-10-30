fafee30e0a34cc12ccbabb641e95692b9ee71345
deniskorbakov <korbakovd@gmail.com>
2024-10-30 23:00:19 +0300

    feat: create header

--- src/components/Commons/Header.vue
+++ src/components/Commons/Header.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
-
+import {ClockIcon, MagnifyingGlassIcon, BellAlertIcon} from '@heroicons/vue/24/solid'
 </script>
 
 <template>
   <header>
-    header
+    <ClockIcon class="logo"/>
+    <h1 class="header-title">Leads</h1>
+    <button class="button-create">Add new lead</button>
+    <MagnifyingGlassIcon class="size-6"/>
+    <BellAlertIcon class="size-6"/>
   </header>
 </template>
 
-<style scoped>
+<style scoped lang="postcss">
+header {
+  @apply flex mt-10 items-center flex-nowrap gap-10
+}
 
+.logo {
+  @apply size-16 text-violet-600 flex-none w-14
+}
+
+.header-title {
+  @apply text-2xl font-bold flex-1 w-32
+}
+
+.button-create {
+  @apply bg-violet-600 px-8 py-2 rounded-2xl
+}
 </style>
