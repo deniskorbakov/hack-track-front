e5baa379985983bfc3f2656c62de15da66726508
deniskorbakov <korbakovd@gmail.com>
2024-11-03 14:25:57 +0300

    feat: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ export default {
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'primary': '#a6206c',
+      'secondary': '#2c2c2c',
+      'background': '#232323',
+    },
     extend: {},
   },
   plugins: [],
