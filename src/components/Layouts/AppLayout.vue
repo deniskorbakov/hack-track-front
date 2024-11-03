88e30747cce9f198dc37ca09e535ea2d6aabe138
deniskorbakov <korbakovd@gmail.com>
2024-11-03 14:08:29 +0300

    feat: add base struct elements

--- src/components/Layouts/AppLayout.vue
+++ src/components/Layouts/AppLayout.vue
@@ -4,8 +4,20 @@ import SideBar from "../Commons/SideBar.vue";
 </script>
 <template>
   <Header/>
-  <SideBar/>
-  <main>
-    <slot/>
-  </main>
+  <div class="content">
+    <SideBar/>
+    <main class="page">
+      <slot/>
+    </main>
+  </div>
 </template>
+<style lang="postcss">
+.content {
+  @apply flex gap-8 h-dvh
+}
+
+.page {
+  @apply mt-10 bg-gray-800 rounded-2xl
+  px-10 py-14 w-full h-auto
+}
+</style>
