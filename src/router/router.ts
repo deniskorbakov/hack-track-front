9a43b9c542c48c798b775a07dab6dfb2e392caa1
deniskorbakov <korbakovd@gmail.com>
2024-11-04 15:30:00 +0300

    feat: add new path add image

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,14 +1,58 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 
 import Home from "../pages/Home.vue";
-import Test from "../pages/Test.vue";
+import Profile from "../pages/Profile.vue";
+import Team from "../pages/Team.vue";
+import Task from "../pages/Task.vue";
+import Statistic from "../pages/Statistic.vue";
+import {
+    UserGroupIcon,
+    PresentationChartLineIcon,
+    HomeIcon,
+    RectangleStackIcon,
+    UserIcon
+} from "@heroicons/vue/24/solid";
 
 const routes = [
-    { path: "/", component:  Home},
-    { path: "/test", component: Test },
+    {
+        path: "/",
+        component: Home,
+        meta: {
+            icon: HomeIcon
+        }
+    },
+    {
+        path: "/profile",
+        component: Profile,
+        meta: {
+            icon: UserIcon
+        }
+    },
+    {
+        path: "/teams",
+        component: Team,
+        meta: {
+            icon: UserGroupIcon
+        }
+    },
+    {
+        path: "/tasks",
+        component: Task,
+        meta: {
+            icon: RectangleStackIcon
+        }
+    },
+    {
+        path: "/statistic",
+        component: Statistic,
+        meta: {
+            icon: PresentationChartLineIcon
+        }
+    },
 ];
 
 const router = createRouter({
+    linkActiveClass: 'text-primary',
     history: createWebHistory(),
     routes,
 });
