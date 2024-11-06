4d1b9ecd07360ab687bbac9ec23b445ced4efe41
deniskorbakov <korbakovd@gmail.com>
2024-11-07 00:16:47 +0300

    feat: add base new section for sidebar

--- src/components/Commons/SideBar.vue
+++ src/components/Commons/SideBar.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-
-
-import {useRouter} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 
 const router = useRouter()
+const route = useRoute()
+
+function isActiveLink (path: string): boolean {
+  return route.path === path
+}
 </script>
 
 <template>
-  <aside class="inline-block">
+  <aside class="bg-gray max-w-20 p-5">
     <ul class="links">
-      <li class="links-item" v-for="(item, key) in router.getRoutes()" :key="key">
+      <li class="links__item" :class="isActiveLink(item.path) ? 'links__item-active' : ''" v-for="(item, key) in router.getRoutes()" :key="key">
         <RouterLink :to="item.path">
           <component :is="item.meta.icon"/>
         </RouterLink>
@@ -19,15 +22,14 @@ const router = useRouter()
 </template>
 
 <style scoped lang="postcss">
-.links {
-  @apply mt-16 pl-1
-}
+  .links {
+    @apply space-y-4
+  }
+  .links__item {
+    @apply size-10 text-gray-300 p-2 hover:text-white
+  }
 
-.links-item {
-  @apply size-8 mb-8
-}
-
-.links-item-active {
-  @apply text-primary
-}
+  .links__item-active {
+    @apply text-white bg-gray-200 shadow-inner shadow-md-left drop-shadow-xl rounded-lg shadow-white/70 hover:shadow-white
+  }
 </style>
