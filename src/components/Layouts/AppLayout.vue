65e37aa11847cdc8b38b958c33ceed7e1a025c26
deniskorbakov <korbakovd@gmail.com>
2024-11-07 00:16:31 +0300

    fix: delete extra code

--- src/components/Layouts/AppLayout.vue
+++ src/components/Layouts/AppLayout.vue
@@ -1,23 +1,11 @@
 <script setup lang="ts">
-import Header from "../Commons/Header.vue";
 import SideBar from "../Commons/SideBar.vue";
 </script>
 <template>
-  <Header/>
-  <div class="content">
-    <SideBar/>
-    <main class="page">
-      <slot/>
-    </main>
-  </div>
+  <SideBar/>
+  <main>
+    <slot/>
+  </main>
 </template>
 <style lang="postcss">
-.content {
-  @apply flex gap-8 h-dvh
-}
-
-.page {
-  @apply mt-10 bg-background rounded-2xl
-  px-10 py-14 w-full h-auto
-}
 </style>
